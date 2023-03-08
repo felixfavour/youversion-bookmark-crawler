5289ae7fc83981dc538e4810b338b71394c22886
felixfavour <felixfavour0@gmail.com>
2023-03-08 09:18:54 +0100

    feat: added UI and backend

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const bodyParser = require('body-parser')
 const path = require("path")
 const app = express()
 const port = process.env.PORT || 4400
 
 // use Parser Middleware
 app.use(express.json())
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // use Logger middleware
 app.use(function (req, res, next) {
@@ -15,6 +17,13 @@ app.use(function (req, res, next) {
 
 app.get('/', async function (req, res, next) {
   return res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.post('/', urlencodedParser, async function (req, res, next) {
+  const promise = await fetch('https://my.bible.com/users/fffelixfavour0/_cards.json?kind=bookmark&page=1')
+  const data = await promise.json()
+  console.log('DATA', data)
+  return res.send(data)
 })
 
 // Listen to port
